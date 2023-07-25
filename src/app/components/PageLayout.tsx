10000d33cdddee1794da3f3e2fa5d7bbce4abaf4
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-07-25 15:21:38 +0530

    side-bar-added

--- src/app/components/PageLayout.tsx
+++ src/app/components/PageLayout.tsx
@@ -1,14 +1,23 @@
 'use client'
 import Footer from './Footer'
 import Header from './Header'
-const PageLayout = ({children} : any) => {
+import SideBar from './SideBar'
+const PageLayout = ({ children }: any) => {
 
-  return (<>
-
-    <Header/>
-      {children}
-    <Footer/>
-        </>    
+  return (
+    <>
+      <Header />
+      <div className='w-full md:grid md:grid-cols-5 mt-16 h-[75vh] bg-[#e9ecef]'>
+        {/* we need to hide this in login and signup page , check if user is present in context if and then use conditional rendering */}
+        <section className='hidden col-span-1 md:flex h-full  w-full md:flex-col'>
+          <SideBar/>
+        </section>
+        <main className=' flex flex-col' >
+          {children}
+        </main>
+      </div>
+      <Footer />
+    </>
   )
 }
 export default PageLayout
---
+++ src/app/components/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <section className='bg-white text-black px-5 py-3 h-full m-5 rounded-xl flex flex-col items-center'>
+        <div className='font-bold text-xl py-4'>
+            OneData
+        </div>
+        <hr className=' border border-t-0 border-[#ced4da] w-full'/>
+    </section>
+  )
+}
